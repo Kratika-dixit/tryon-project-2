4e67710b7be17cb266689485a30ea51f679230c6
Kratika Dixit <kratikadixitkhajuraho@gmail.com>
2026-05-31 11:10:32 +0530

    added backgound.js

--- tryon-extension/background.js
+++ tryon-extension/background.js
@@ -1,3 +1,15 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('tryon extension installed');
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'saveImage' && message.imageUrl) {
+    // Get existing saved images, then append the new one
+    chrome.storage.local.get(['selectedImages'], (result) => {
+      const images = result.selectedImages || [];
+      images.push(message.imageUrl);
+      chrome.storage.local.set({ selectedImages: images }, () => {
+        console.log('Image saved to storage:', message.imageUrl);
+        sendResponse({ success: true });
+      });
+    });
+    // Return true to indicate we'll call sendResponse asynchronously
+    return true;
+  }
 });
